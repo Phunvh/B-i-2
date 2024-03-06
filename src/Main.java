c2e73b117e8769b9ee37e26c757968447107c0de
phunvh <nguyenvanhoangphulq@gmail.com>
2024-03-06 21:22:07 +0700

    add hi

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello dsgjgsvdgjgvds!");
+        System.out.println("Hello world!");
+        System.out.println("hello nhóc");
     }
 }
